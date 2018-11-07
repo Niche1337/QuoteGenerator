d69dfbfca1298777e7ac6d530735bab1a9886190
Nitesh Parmar <niteshp2007@gmail.com>
2018-11-07 22:21:17 +0000

    Update js/script.js
    Incorrectly created the html string. Fixed with correct sytnax. Generates quotes.

--- js/script.js
+++ js/script.js
@@ -1,9 +1,6 @@
-/******************************************
-Treehouse Techdegree:
-FSJS project 1 - A Random Quote Generator
-******************************************/
 
-// Study guide for this project - https://drive.google.com/file/d/1s5grutGuQFwJcQP8bFwEI69Q8FCkGdDk/view?usp=sharing
+
+
 
 /*Array of objects containing quotes, authors, year of publication and citations */
 var quotes = [{quote: '"Don\'t cry because it\'s over, smile because it happened."', author: "Dr.Seuss"
@@ -28,40 +25,20 @@ function getRandomQuote(quotes) {
 
 console.log(getRandomQuote(quotes));
 
-/***
-  Create the `printQuote` function to: 
-   - call the `getRandomQuote` function and assign it to a variable.
-   - use the properties of the quote object stored in the variable to 
-     create your HTML string.
-   - use conditionals to make sure the optional properties exist before 
-     they are added to the HTML string.
-   - set the `innerHTML` of the `quote-box` div to the HTML string. 
-***/
-
 function printQuote() {
 	var htmlText;
 	var quoteObj = getRandomQuote(quotes);
-	htmlText = '<p class="quote"> quoteObj.quote </p>';
-	htmlText += '<p class="source"> quoteObj.author';
+	htmlText = '<p class="quote">' + quoteObj.quote + '</p>';
+	htmlText += '<p class="source">' + quoteObj.author;
 	if (quoteObj.citation != null) {
-		htmlText += '<span class="citation"> quoteObj.citation </span>'
+		htmlText += '<span class="citation">' + quoteObj.citation + '</span>'
 	}
 	if (quoteObj.year != null) {
-		htmlText += '<span class="year"> quoteObj.year </span>'
+		htmlText += '<span class="year">' + quoteObj.year + '</span>';
 	}
 	htmlText += '</p>'
-	console.log(htmlText)
+	document.getElementById("quote-box").innerHTML = htmlText;
 }
-printQuote()
-
-/***
-  When the "Show another quote" button is clicked, the event listener 
-  below will be triggered, and it will call, or "invoke", the `printQuote` 
-  function. So do not make any changes to the line of code below this 
-  comment.
-***/
+printQuote();
 
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
-
-
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
